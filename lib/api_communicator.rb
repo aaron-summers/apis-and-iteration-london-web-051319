c782f0246eaa8ec82153cefe161bd95574201b0c
Aaron Summers <aaron.summers2797@gmail.com>
2019-05-14 11:37:43 +0100

    accepts user input in the search function

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,8 +4,9 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_string = RestClient.get('http://www.swapi.co/api/people/#{user_input}')
   response_hash = JSON.parse(response_string)
+
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
